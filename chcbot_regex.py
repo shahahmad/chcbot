64ba75b35934a03a544798032df51235d9874aac
shahahmad <ahmadshah@MacbookPro14.local>
2017-10-07 16:50:02 -0400

    Added code comments

--- chcbot_regex.py
+++ chcbot_regex.py
@@ -3,11 +3,13 @@
 def chcbot(input_str):
     import re, string, csv
     input_str = input_str.lower()
+    #remove all punctuation from input string besides @
     punctuation = string.punctuation.replace('@','')
     input_str = input_str.translate(None, punctuation)
     #set up primary and secondary keys for commands
     keyword_dict = {}
     import csv
+    #open the input file and store the data in a dictionary
     with open('input.txt', 'rb') as f:
         reader = csv.reader(f, delimiter="\t")
         d = list(reader)
@@ -15,14 +17,18 @@ def chcbot(input_str):
             tab_row = re.split(r'\\t+', row[0])
             secondary_keys = tab_row[1]
             keyword_dict[tab_row[0]] = {secondary_keys:tab_row[2]}
+    #split input words into a list using a space as a delimiter
     primary_keys = keyword_dict.keys()
     input_list = input_str.split()
+    #if input stirng doesn't containg primary keys then return nothing
     if bool(set(input_list) & set(primary_keys)) == False:
         return
+    #check if @chainbot in input string
     if re.search("@chainbot", input_str) != None:
         chainbot_chk = 1
     else:
         chainbot_chk = 0
+    #if @chainbot is in input string, then generate reponse based on primary keys
     if chainbot_chk == 1:
         overlap_str_primary = list(set(input_list) & set(primary_keys))
         if len(overlap_str_primary) == 0:
@@ -31,9 +37,12 @@ def chcbot(input_str):
             overlap_str_primary = list(overlap_str_primary)[0]
             if overlap_str_primary in primary_keys:
                 return(keyword_dict[overlap_str_primary].values()[0])
+    #if @chainbot not in input string then check primary keys for overlap
     else:
         overlap_str_primary = set(input_list) & set(primary_keys)
         overlap_str_primary = list(overlap_str_primary)[0]
+        #if any primary key is in input string check if its associated secondary
+        #keys are as well
         if overlap_str_primary in primary_keys:
             secondary_keys = keyword_dict[overlap_str_primary].keys()[0].split(',')
             if len(secondary_keys) == 0:
